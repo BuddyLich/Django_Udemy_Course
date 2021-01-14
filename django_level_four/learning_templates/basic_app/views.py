7b874159064098621ada7cb266f3c9363a5dcedf
Zhen <zhengzhen048@gmail.com>
2021-01-14 22:42:21 +1100

    chapter 148

--- django_level_four/learning_templates/basic_app/views.py
+++ django_level_four/learning_templates/basic_app/views.py
@@ -7,11 +7,12 @@ def index(request):
         "text": "test text",
         "number": "114514",
         }
-    return render(request,'basic_app/index.html', context_dict)
+    return render(request, 'basic_app/index.html', context_dict)
 
 
 def other(request):
     return render(request, 'basic_app/other.html')
+
 
 def relative(request):
     return render(request, 'basic_app/relative_url_templates.html')
