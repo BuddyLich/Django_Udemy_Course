e073e1524ba12940d6ae7cd4e87bdc482f7079e9
Zhen <zhengzhen048@gmail.com>
2021-01-15 21:36:38 +1100

    chapter 154

--- django_level_five/learning_users/basic_app/views.py
+++ django_level_five/learning_users/basic_app/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from basic_app.forms import UserForm, UserProfileInfoForm
 
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
 
 def index(request):
     return render(request, 'basic_app/index.html')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+@login_required
+def special(request):
+    return HttpResponse("You are logged in, nice!")
 
 
 def register(request):
@@ -40,3 +56,27 @@ def register(request):
         "registered": registered
     }
     return render(request, 'basic_app/registration.html', context_dict)
+
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+
+            else:
+                return HttpResponse("ACCOUNT NOT ACTIVE")
+
+        else:
+            print("Someone tried to login and failed!")
+            print(f"Username: {username} and Password: {password}")
+            return HttpResponse("Invalid login details supplied!")
+
+    else:
+        return render(request, 'basic_app/login.html', {})
