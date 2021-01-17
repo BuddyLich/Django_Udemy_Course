f462555dfec8797ff757ccf64d887299e58a4086
Zhen <zhengzhen048@gmail.com>
2021-01-17 16:41:35 +1100

    chapter 167
    setting up forms and settings.py

--- blog_project/mysite/blog/models.py
+++ blog_project/mysite/blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=128)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
-    published_date = models.DateTimeField(blan=True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -24,7 +24,7 @@ class Post(models.Model):
         return self.title
 
 
-class Comments(models.Model):
+class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE)
     author = models.CharField(max_length=128)
     text = models.TextField()
